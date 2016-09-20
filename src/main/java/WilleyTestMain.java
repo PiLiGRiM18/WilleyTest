2cda0ebf6ffa945ca5b2b9ccde6bf00d56bea634
piligrim <piligrim18@gmail.com>
2016-09-20 15:02:28 +0300

    Actions 1 -> 12 Done!

--- src/main/java/WilleyTestMain.java
+++ src/main/java/WilleyTestMain.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by piligrim on 17.09.16.
@@ -62,14 +65,14 @@ public class WilleyTestMain {
                             "Action 2 CHECKED!\n");
                     countCheck++;
                 } catch (Exception e) {
-                    System.out.println("Action 2 FAILED!");
+                    System.out.println("Action 2 FAILED!\n");
                     countFail++;
                     e.printStackTrace();
                 }
             }
 
         } catch (Exception e) {
-            System.out.println("Action 2 FAILED!");
+            System.out.println("Action 2 FAILED!\n");
             countFail++;
             e.printStackTrace();
         }
@@ -111,7 +114,7 @@ public class WilleyTestMain {
                     System.out.println("Action 4:\n" +
                             "Check “Resources For” menu on the left\n" +
                             "8 items are displayed in the menu\n" +
-                            "Items are “Authorts”, “Librarians”, “Booksellers”, “Instructors”, “Students” ,”Government Employees”, “Societies”, “Corporate Partners”" +
+                            "Items are “Authorts”, “Librarians”, “Booksellers”, “Instructors”, “Students” ,”Government Employees”, “Societies”, “Corporate Partners”\n" +
                             "Action 4 CHECKED!\n");
                     countCheck++;
                 } catch (Exception e) {
@@ -143,7 +146,7 @@ public class WilleyTestMain {
                             "Action 5 FAILED!\n");
                     countFail++;
                 } catch (Exception e) {
-                    // TODO: проверить different style
+                    // TODO: is classname "active" different style?
                     System.out.println("Action 5:\n" +
                             "Check “Students” item is selected\n" +
                             "“Students” item has different style\n" +
@@ -183,21 +186,21 @@ public class WilleyTestMain {
             driver.findElement(By.name("submitButton")).click();
             Alert alert1 = driver.switchTo().alert();
             if (alert1.getText().contains("Please enter email address")) {
-                System.out.println("Action 7\n" +
+                System.out.println("Action 7:\n" +
                         "Find “Sign up to receive Wiley updates” line and input field next to it. Do not enter anything and click arrow button\n" +
                         "Check that alert appeared\n" +
                         "Check that alert text is “Please enter email address”\n" +
                         "Action 7 CHECKED!\n");
                 countCheck++;
             } else {
-                System.out.println("Action 7\n" +
+                System.out.println("Action 7:\n" +
                         "the alert text is not “Please enter email address” its " + "“" + alert1.getText() + "”\n" +
                         "Action 7 FAILED!\n");
                 countFail++;
             }
             alert1.accept();
         } catch (Exception e) {
-            System.out.println("Action 7 FAILED!");
+            System.out.println("Action 7 FAILED!\n");
             countFail++;
             e.printStackTrace();
         }
@@ -224,7 +227,7 @@ public class WilleyTestMain {
             }
             alert2.accept();
         } catch (Exception e) {
-            System.out.println("Action 8 FAILED!");
+            System.out.println("Action 8 FAILED!\n");
             countFail++;
             e.printStackTrace();
         }
@@ -234,44 +237,85 @@ public class WilleyTestMain {
             WebElement searchInputField = driver.findElement(By.name("query"));
             searchInputField.sendKeys("for dummies");
             driver.findElement(By.className("search-form-submit")).click();
+            driver.findElement(By.id("search-results"));
+            System.out.println("Action 9:\n" +
+                    "Find search input in the top of the page. Enter “for dummies” to the input field and press search icon next to the input field.\n" +
+                    "Check that list of items appeared\n" +
+                    "Action 9 CHECKED!\n");
         } catch (Exception e) {
-            System.out.println("Action 9 FAILED!");
+            System.out.println("Action 9 FAILED!\n");
             countFail++;
             e.printStackTrace();
         }
+
         // Action 10
         try {
-
+            List<WebElement> searchListEl = driver.findElement(By.id("search-results")).findElements(By.className("product-listing"));
+            int randomLinkId = new Random().nextInt(searchListEl.size());
+            String randomLinkText = searchListEl.get(randomLinkId).findElement(By.className("product-title")).getText();
+            driver.findElement(By.linkText(randomLinkText)).click();
+            driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
+            if (driver.findElement(By.className("productDetail-title")).getText().contains(randomLinkText)) {
+                System.out.println("Action 10:\n" +
+                        "Click random item link (link with book title)\n" +
+                        "Check that page with header equal to the title you clicked is displayed\n" +
+                        "Action 10 CHECKED!\n");
+                countCheck++;
+            } else {
+                System.out.println("Action 10 FAILED!\n");
+                countFail++;
+            }
         } catch (Exception e) {
-            System.out.println("Action 10 FAILED!");
+            System.out.println("Action 10 FAILED!\n");
+            countFail++;
             e.printStackTrace();
         }
+
         // Action 11
         try {
-
+            WebElement home = driver.findElement(By.linkText("Home"));
+            home.click();
+            System.out.println("Action 11:\n" +
+                    "Click “Home” link at the top navigation menu\n" +
+                    "Action 11 CHECKED!\n");
+            countCheck++;
         } catch (Exception e) {
-            System.out.println("Action 11 FAILED!");
+            System.out.println("Action 11 FAILED!\n");
+            countFail++;
             e.printStackTrace();
         }
+
         // Action 12
         try {
-
+            WebElement institutions = driver.findElement(By.linkText("Institutions"));
+            institutions.click();
+            ArrayList<String> tabsList = new ArrayList<String>(driver.getWindowHandles());
+            Thread.sleep(4000);
+            driver.switchTo().window(tabsList.get(1));
+            // TODO: доделать, открывается сначала http://wileyedsolutions.com/ потом идет перенаправление на
+            if (driver.getCurrentUrl().contains("http://wileyedsolutions.com/")) {
+                System.out.println("Action 12:\n" +
+                        "Click “Institutions” icon under Resources sub-header\n" +
+                        "Check http://wileyedsolutions.com/ is opened in new window (or tab)\n" +
+                        "Action 12 CHECKED!\n");
+                countCheck++;
+            } else {
+                System.out.println("Action 12:\n" +
+                        "the http://wileyedsolutions.com/ is not opened in new window (or tab) it's " + driver.getCurrentUrl() + "\n" +
+                        "Action 12 FAILED!\n");
+                countFail++;
+            }
         } catch (Exception e) {
-            System.out.println("Action 12 FAILED!");
+            System.out.println("Action 12 FAILED!\n");
+            countFail++;
             e.printStackTrace();
         }
 
-        try {
-
-        } catch (Exception e) {
-            System.out.println("Action 10 FAILED!");
-            e.printStackTrace();
-        }
         System.out.println("=============================\n" +
                 "**-= ALL TESTS COMPLETE =- **\n" +
                 countCheck + " actions is checked,\n" +
                 countFail + " actions is failed\n" +
                 "=============================");
-        driver.quit();
+//        driver.quit();
     }
 }
